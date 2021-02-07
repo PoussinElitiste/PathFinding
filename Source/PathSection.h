b57d56cf0e704f7dc3bda59112dbaa069697550d
Khalil Hajlaoui <khalil.hajlaoui@gmail.com>
2021-02-07 15:48:28 -0500

    move weight to pathSection

--- Source/PathSection.h
+++ Source/PathSection.h
@@ -4,9 +4,20 @@
 class PathSection
 {
 public:
-    PathSection(PathNode *n1, PathNode *n2, double iWeight):
-        mNode1(n1), mNode2(n2), mWeigt(iWeight)
-    {}
+    PathSection(PathNode *n1, PathNode *n2):
+        mNode1(n1), mNode2(n2), mWeigt(std::numeric_limits<double>::max())
+    {
+        if (!n1 || !n2)
+        {
+            printf("[ERROR] invalid node");
+            return;
+        }
+        else
+        {
+            Vertex distance = n1->GetVertex() - n2->GetVertex();
+            mWeigt = distance.length();
+        }
+    }
 
 private:
     PathNode *mNode1, *mNode2;
