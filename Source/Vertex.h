2d43f71bb65baee8cb8220392f47c62f9ce36f13
Khalil Hajlaoui <khalil.hajlaoui@gmail.com>
2021-02-07 14:46:23 -0500

    add vertex operations

--- Source/Vertex.h
+++ Source/Vertex.h
@@ -1,19 +1,26 @@
 #ifndef VERTEX_H
 #define VERTEX_H
 
+#include <cmath>
+
 struct Vertex
 {
     Vertex() : x{}, y{}, z{}
     {}
 
-    Vertex(float iX, float iY, float iZ) :
-        x(iX),
-        y(iY),
-        z(iZ)
-    {
-    }
+    Vertex(double iX, double iY, double iZ) :
+        x(iX), y(iY), z(iZ)
+    {}
 
-    float x, y, z;
+    inline double length() const { return std::sqrt(x * x + y * y + z * z); }
+
+    inline double length2() const { return x * x + y * y + z * z; }
+
+    double x, y, z;
 };
+
+inline Vertex operator+(const Vertex& v0, const Vertex& v1) { return Vertex(v0.x + v1.x, v0.y + v1.y, v0.z + v1.z); }
+
+inline Vertex operator-(const Vertex& v0, const Vertex& v1) { return Vertex(v0.x - v1.x, v0.y - v1.y, v0.z - v1.z); }
 
 #endif // VERTEX_H
