9b5bdcc7532d0ece4dfb520d734b566550ab1475
Khalil Hajlaoui <khalil.hajlaoui@gmail.com>
2021-02-07 21:38:08 -0500

    set Distance on PathNode link

--- Source/PathNode.h
+++ Source/PathNode.h
@@ -5,9 +5,12 @@
 #include <memory>
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include <limits>
 
 class PathNode;
 typedef std::vector<PathNode*> PathNodes;
+typedef std::unordered_map<PathNode*, double> NodeDistances;
 
 class PowerUp;
 typedef std::vector<PowerUp*> PowerUps;
@@ -22,9 +25,15 @@ public:
     
     ~PathNode() = default;
 
-    void AddLink(PathNode *pathNode)
+    void AddLink(PathNode *pathNode, double weight)
     {
-        mLinks.push_back(pathNode);
+        if (!pathNode)
+        {
+            printf("[ERROR] [AddLink] Invalid node");
+            return;
+        }
+       
+        mDistances.emplace(pathNode, weight);
     }
     
     void RemoveLink(PathNode *pathNode)
@@ -59,6 +68,15 @@ public:
         return mLinks;
     }
 
+    double GetDistance(PathNode* pathNode) const
+    {
+        if (mDistances.count(pathNode) > 0)
+        {
+            return mDistances.at(pathNode);
+        }
+        return std::numeric_limits<double>::max();
+    }
+
     const PowerUps& GetPowerUps() const
     {
         return mPowerUps; 
@@ -69,6 +87,7 @@ protected:
     const std::string   mName;
 
     PathNodes           mLinks;
+    NodeDistances       mDistances;
     PowerUps            mPowerUps;
 };
 
